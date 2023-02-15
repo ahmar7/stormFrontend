ffe4306f0f7a376b4e3c2788387f16f05eb1af9d
ahmar7 <ahmarjabbar7@gmail.com>
2023-02-16 01:13:55 +0500

    update

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,15 +1,76 @@
 import React from 'react';
 import { Slide } from '../utils/allImg';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './Testimonial.css'
 const Testimonial = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        speed: 500,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+        ],
+    };
     return (
-        <div className='testimonial-sec'> 
+        <div className='testimonial-sec'>
             <div className="bg-img">
                 <div className="test-overlay text-center">
 
                     <h1 className="text-center test-heading">Vores <span className="orange">Kunder siger</span></h1>
-<p className="test-des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugiat et.</p>
+                    <p className="test-des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugiat et.</p>
                     <img className='text-center' src={Slide} alt="" />
+
+                    <Slider {...settings}>
+                        <div>
+                            <h3>1</h3>
+                        </div>
+                        <div>
+                            <h3>2</h3>
+                        </div>
+                        <div>
+                            <h3>3</h3>
+                        </div>
+                        <div>
+                            <h3>4</h3>
+                        </div>
+                        <div>
+                            <h3>5</h3>
+                        </div>
+                        <div>
+                            <h3>6</h3>
+                        </div>
+                    </Slider>
                 </div>
             </div>
         </div>
